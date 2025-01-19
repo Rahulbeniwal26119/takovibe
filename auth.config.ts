7e49e17d8ff335dbbc6eab236dc332dd383134c2
rahul <rahulbeniwal18112002@gmail.com>
2025-01-19 22:03:20 +0530

    added auth.config.ts

---
+++ auth.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from '@auth/core';
+import Google from '@auth/core/providers/google';
+
+export default {
+  providers: [
+    Google({
+      clientId: import.meta.env.PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: import.meta.env.GOOGLE_SECRET_KEY,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.accessToken = token.accessToken;
+        session.refreshToken = token.refreshToken;
+      }
+      return session;
+    },
+  },
+} satisfies Config; 
--- src/pages/api/auth/[...auth].ts
+++ src/pages/api/auth/[...auth].ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'auth-astro';
 import Google from '@auth/core/providers/google';
 
+export const prerender = false;
+
 export const authConfig = defineConfig({
   providers: [
     Google({
